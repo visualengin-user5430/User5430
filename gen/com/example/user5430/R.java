661dd7ff24bb0a752852c1a4bebfbb7fab69c6af
Marc <muixi.marc@gmail.com>
2015-01-15 12:35:57 +0100

    UI

--- gen/com/example/user5430/R.java
+++ gen/com/example/user5430/R.java
@@ -10,6 +10,9 @@ package com.example.user5430;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int list_text_color=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,25 +21,34 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo1=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090006;
+        public static final int custom_lv=0x7f090005;
+        public static final int imageView1=0x7f090000;
+        public static final int linearLayout=0x7f090001;
+        public static final int listView1=0x7f090002;
+        public static final int progressBar1=0x7f090003;
+        public static final int textView1=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int custom_list_view=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int logo_desc=0x7f060003;
     }
     public static final class style {
         /** 
@@ -60,10 +72,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/example/user5430/MainActivity.java
+++ src/com/example/user5430/MainActivity.java
@@ -1,18 +1,87 @@
 package com.example.user5430;
 
+/*import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;*/
+
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+//import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+/*import android.view.View;
+import android.widget.ArrayAdapter;*/
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-
+	  private ProgressBar spinner;
+	  private ListView listview;
+	  private LinearLayout linearLayout;
+	  private TextView tV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        listview = (ListView) findViewById(R.id.listView1);
+        linearLayout = (LinearLayout)findViewById(R.id.linearLayout);
+        tV = (TextView)findViewById(R.id.textView1);
+        if(!isOnline(this)){
+        	tV.setText("Internet connection not disponible");
+        	spinner.setVisibility(View.GONE);
+        }
+        else{//isOnline//
+        	tV.setText("OK");
+        }
+        
+        /*String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
+
+            final ArrayList<String> list = new ArrayList<String>();
+            for (int i = 0; i < values.length; ++i) {
+              list.add(values[i]);
+            }
+            final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                   R.layout.custom_list_view, list);
+                listview.setAdapter(adapter);*/
     }
+    /*private class StableArrayAdapter extends ArrayAdapter<String> {
+
+	    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+	    public StableArrayAdapter(Context context, int textViewResourceId,
+	        List<String> objects) {
+	      super(context, textViewResourceId, objects);
+	      for (int i = 0; i < objects.size(); ++i) {
+	        mIdMap.put(objects.get(i), i);
+	      }
+	    }
+
+	    @Override
+	    public long getItemId(int position) {
+	      String item = getItem(position);
+	      return mIdMap.get(item);
+	    }
+
+	    @Override
+	    public boolean hasStableIds() {
+	      return true;
+	    }
+
+	  }*/
 
 
     @Override
@@ -33,4 +102,19 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+    private boolean isOnline(Context context) {
+    	ConnectivityManager cm =
+    	        (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    	 
+    	NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+    	boolean isConnected = activeNetwork != null &&
+    	                      activeNetwork.isConnected();
+    	//Log.d("CON", "active connection: " + isConnected);
+    	return isConnected;
+    }
+    /*public void sends(View view) {
+        spinner.setVisibility(View.GONE);
+  	  	linearLayout.setVisibility(View.VISIBLE);
+    }*/
 }
